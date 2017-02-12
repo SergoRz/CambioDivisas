4cea5c7eae3fa6bc8b6362a0786adb48fa7a0f8b
ecabab <ecabab@gmail.com>
2017-02-12 13:59:46 +0100

    bbdd

--- app/src/main/java/com/example/dam212/cambiodivisas/AccesoBD.java
+++ app/src/main/java/com/example/dam212/cambiodivisas/AccesoBD.java
@@ -1,9 +1,40 @@
 package com.example.dam212.cambiodivisas;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by EmilioCB on 12/02/2017.
  */
 
-public class AccesoBD {
+public class AccesoBD extends SQLiteOpenHelper{
+    public static final int DATABASE_VERSION = 1;//Contexto de acción para el helper.
+    public static final String DATABASE_NAME = "divisasDB.db";
+    //Nombre del archivo con extensión .db
 
+    //Sentencia SQL que crea la tabla
+    String sqlCreate = "CREATE TABLE divisas(" +
+            "moneda1 DOUBLE PRIMARY KEY NOT NULL," +
+            "moneda2 DOUBLE PRIMARY KEY NOT NULL," +
+            "valor DOUBLE NOT NULL" +
+            ");";
+
+    public AccesoBD(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(sqlCreate);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void insert(){
+
+    }
 }
--- app/src/main/java/com/example/dam212/cambiodivisas/MainActivity.java
+++ app/src/main/java/com/example/dam212/cambiodivisas/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dam212.cambiodivisas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static android.provider.ContactsContract.CommonDataKinds.Identity.NAMESPACE;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
