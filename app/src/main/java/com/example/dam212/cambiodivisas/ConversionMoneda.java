5a7d9420c56f54efdb574b376a6d401377a88630
SergoRz <sanchez13rz@gmail.com>
2017-02-13 19:04:21 +0100

    SI JODER!

--- app/src/main/java/com/example/dam212/cambiodivisas/ConversionMoneda.java
+++ app/src/main/java/com/example/dam212/cambiodivisas/ConversionMoneda.java
@@ -15,11 +15,17 @@ public class ConversionMoneda {
         this.valor = valor;
     }
 
+    /**
+     * Constructor que contiene los dos tipos de moneda String, String
+     * @param moneda1 moneda de la que se va a hacer la conversion
+     * @param moneda2 moneda a la que se va a hacer la conversion
+     */
     public ConversionMoneda(String moneda1, String moneda2){
         this.moneda1 = moneda1;
         this.moneda2 = moneda2;
     }
 
+    //Getters
     public String getMoneda1() {
         return moneda1;
     }
@@ -30,17 +36,5 @@ public class ConversionMoneda {
 
     public String getMoneda2() {
         return moneda2;
-    }
-
-    public void setMoneda1(String moneda1) {
-        this.moneda1 = moneda1;
-    }
-
-    public void setMoneda2(String moneda2) {
-        this.moneda2 = moneda2;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
     }
 }
