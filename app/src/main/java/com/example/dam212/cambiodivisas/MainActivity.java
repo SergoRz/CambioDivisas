4b1a9cd2698a4cc864652061667492d494f13cb8
ecabab <ecabab@gmail.com>
2017-02-13 17:46:03 +0100

    fd

--- app/src/main/java/com/example/dam212/cambiodivisas/MainActivity.java
+++ app/src/main/java/com/example/dam212/cambiodivisas/MainActivity.java
@@ -130,6 +130,7 @@ public class MainActivity extends Activity {
         @Override
         protected void onProgressUpdate(Void... values) {
             Log.i(TAG, "onProgressUpdate");
+
         }
 
         public void getConversionSOAP() {
